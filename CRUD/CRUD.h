76f60bc58d98587ad63fb8df06d74c7a576902b2
Alexander Zalutskiy <metalhead.sanya@gmail.com>
2016-07-26 10:02:05 +0600

    Initial Commit

---
+++ CRUD/CRUD.h
@@ -0,0 +1,19 @@
+//
+//  CRUD.h
+//  CRUD
+//
+//  Created by Alexander Zalutskiy on 26.07.16.
+//  Copyright © 2016 Alexander Zalutskiy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CRUD.
+FOUNDATION_EXPORT double CRUDVersionNumber;
+
+//! Project version string for CRUD.
+FOUNDATION_EXPORT const unsigned char CRUDVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CRUD/PublicHeader.h>
+
+
